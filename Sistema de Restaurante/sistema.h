650c89d47c1935eb933173d29bac5f161300ec89
GusVOnHell <gustavotatugg@gmail.com>
2026-09-20 23:52:45 -0300

    mudança no sistema.h

--- Sistema de Restaurante/sistema.h
+++ Sistema de Restaurante/sistema.h
@@ -5,9 +5,18 @@ typedef struct PRODUTO{
     int codigo_interno;
     char nome[50];
     char categoria[50];
-    float preco;
+   float preco;
 }produto;
 
+typedef struct Pedido{
+    int numero;
+    Produto produto;
+    int quantidade;
+}Tpedido, *Ppedido, **Mpedido;
+
 void listarCardapio(produto cardapio[],int qtd);
+void consultar_pedido(Pedido pedido);
+void cadastrarPedido(Tpedido pedidos[], Produto produtos[],int totalProdutos);
+void calculoPedido(Ppedido listaPedidos, int nPedido);
 
 #endif

df48d81da22f294de0f0b895956a5a40c9c5ecf4
clevertonkauasilvasousa384-gif <clevertonkauasilvasousa384@gmail.com>
2026-09-21 01:42:25 -0300

    Update sistema.c

--- Sistema de Restaurante/sistema.c
+++ Sistema de Restaurante/sistema.c
@@ -19,7 +19,7 @@ void consultar_pedido(PPedido pedido){
     qtde_pedi: guarda o número total de pedidos
     pedido: struct com os dados dos pedidos de cada mesa
     */
-    int num_mesa, qtde_pedi = 50, j;
+    int num_mesa, qtde_pedi = 50, j = 0;
 
     //registro do número da mesa
     

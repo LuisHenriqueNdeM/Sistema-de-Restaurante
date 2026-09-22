bbdaa82ee797f09cb6c1ec6d6e288fbb57c0ea9f
GusVOnHell <gustavotatugg@gmail.com>
2026-09-21 23:18:12 -0300

    sistema.h

--- Sistema de Restaurante/sistema.h
+++ Sistema de Restaurante/sistema.h
@@ -5,18 +5,21 @@ typedef struct PRODUTO{
     int codigo_interno;
     char nome[50];
     char categoria[50];
-   float preco;
-}produto;
+    float preco;
+} produto;
 
-typedef struct Pedido{
-    int numero;
-    Produto produto;
+typedef struct PEDIDO{
+    int numero;       // número da mesa (0 = posição livre)
+    produto produto;  // produto pedido
     int quantidade;
-}Tpedido, *Ppedido, **Mpedido;
+} Tpedido, *Ppedido, **Mpedido;
 
-void listarCardapio(produto cardapio[],int qtd);
-void consultar_pedido(Pedido pedido);
-void cadastrarPedido(Tpedido pedidos[], Produto produtos[],int totalProdutos);
-void calculoPedido(Ppedido listaPedidos, int nPedido);
+void Cadastrar(int totalProdutos, produto *produtos);
+void listarCardapio(produto cardapio[], int qtd);
+void cadastrarPedido(Tpedido pedidos[], produto produtos[], int totalProdutos);
+void consultar_pedido(Ppedido pedido);
+void alterar_pedido(Ppedido pedido, produto cardapio[], int qtdCardapio);
+void calculoPedido(Tpedido pedidos[], int nPedido);
+void voltar_menu();
 
 #endif

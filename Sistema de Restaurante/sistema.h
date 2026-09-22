94a513f61151ce748f8e2fdfcfd3302d78929cd2
GusVOnHell <gustavotatugg@gmail.com>
2026-09-21 23:17:38 -0300

    sistema.h
    Removed redundant structures and function declarations related to product and order management.

--- Sistema de Restaurante/sistema.h
+++ Sistema de Restaurante/sistema.h
@@ -5,30 +5,6 @@ typedef struct PRODUTO{
     int codigo_interno;
     char nome[50];
     char categoria[50];
-    float preco;
-} produto;
-
-typedef struct PEDIDO{
-    int numero;       // número da mesa (0 = posição livre) 
-    produto produto;  // produto pedido
-    int quantidade;
-} Tpedido, *Ppedido, **Mpedido;
-
-void Cadastrar(int totalProdutos, produto *produtos);
-void listarCardapio(produto cardapio[], int qtd);
-void cadastrarPedido(Tpedido pedidos[], produto produtos[], int totalProdutos);
-void consultar_pedido(Ppedido pedido);
-void alterar_pedido(Ppedido pedido, produto cardapio[], int qtdCardapio);
-void calculoPedido(Tpedido pedidos[], int nPedido);
-void voltar_menu();
-
-#endif#ifndef SISTEMA_H
-#define SISTEMA_H
-
-typedef struct PRODUTO{
-    int codigo_interno;
-    char nome[50];
-    char categoria[50];
    float preco;
 }produto;
 
